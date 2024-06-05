7e4e39166ed6281ce828667a40d48ccd42efc230
EC2 Default User <ec2-user@ip-10-0-2-42.ap-northeast-1.compute.internal>
2024-06-05 07:37:58 +0000

    [add]slackへメッセージ送信の機能追加

--- src/costNotifier.js
+++ src/costNotifier.js
@@ -1,5 +1,8 @@
+import { IncomingWebhook } from '@slack/webhook'
+
 export async function handler(event, context){
-    return {
-        message: 'Hello Severless Framework'
-    }
+    const slackWebHookUrl = process.env.SLACK_WEBHOOK_URL
+    const slackWebHook = new IncomingWebhook(slackWebHookUrl)
+    
+    await slackWebHook.send('こんにちわ！AWSのlamdaのシステムを使用して送信されたメッセージです。tokuが作りました！成長だね！')
 }
